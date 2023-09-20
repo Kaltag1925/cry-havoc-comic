6f3d6e2e57005049c9540b7001a710becd99185f
Kaltag1925 <pdavisbray@gmail.com>
2023-09-20 09:22:18 -0500

    changed description

--- pages/index.js
+++ pages/index.js
@@ -21,10 +21,10 @@ export default function Home({ allPostsData }) {
         <title>{siteTitle}</title>
       </Head>
       <section className={utilStyles.headingMd}>
-        <p>[Your Self Introduction]</p>
         <p>
-          (This is a sample website - you’ll be building a site like this on{' '}
-          <a href="https://nextjs.org/learn">our Next.js tutorial</a>.)
+        Cry Havoc is a comic about the Vietnam War. Based on firsthand accounts told by veterans & their families.
+
+        Dedicated to Mark McVeigh & Dustoff 237
         </p>
       </section>
 
